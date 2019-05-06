5d5403674e829d9c0c3de9b47ebe310f993b879c
Mohammd Rasel <mohammad.rasel@grameensolutions.com>
2019-05-06 17:46:16 +0600

    angularjs

---
+++ angularjs/db/db.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.21 
+MySQL - 5.5.5-10.1.32-MariaDB : Database - angularjs
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`angularjs` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `angularjs`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(80) NOT NULL,
+  `lname` varchar(80) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+LOCK TABLES `users` WRITE;
+
+insert  into `users`(`id`,`fname`,`lname`,`username`) values (1,'Rasel','Ahmed','urasel');
+insert  into `users`(`id`,`fname`,`lname`,`username`) values (2,'Zaman','Sharker','zaman');
+insert  into `users`(`id`,`fname`,`lname`,`username`) values (3,'Didarul','Islam','didar');
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
